2b12159e1321e5a23db0ea74c58b73b89d849fa6
Emir Kamel <p0092396@brookes.ac.uk>
2024-09-09 16:18:46 +0100

    bug fixing for lib file, needs reference for locallib

--- lib.php
+++ lib.php
@@ -23,6 +23,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+global $CFG;
+require_once($CFG->dirroot.'/local/obu_assessment_extensions/locallib.php');
+
 function local_obu_assess_ex_recalculate_due_dates_for_user($user, $group) {
     $assessments = local_obu_get_assessments_by_assessment_group($group);
     foreach ($assessments as $assessment) {
