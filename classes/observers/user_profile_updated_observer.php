b1bd28f752125aa37731e45385d8f8dd8a61d559
Emir Kamel <p0092396@brookes.ac.uk>
2024-10-09 12:55:10 +0100

    updating user profile observer to use a different event to observe

--- classes/observers/user_profile_updated_observer.php
+++ classes/observers/user_profile_updated_observer.php
@@ -32,13 +32,12 @@ global $CFG;
 require_once($CFG->dirroot . '/local/obu_assessment_extensions/locallib.php');
 
 class user_profile_updated_observer {
-    public static function user_profile_updated(\core\event\user_updated $event) {
+    public static function user_profile_updated(\core\event\user_info_field_updated $event) {
         $eventData = $event->get_data();
 
-        $oldProfile = $eventData['other']['oldprofile'] ?? [];
-        $newProfile = $eventData['other']['profile'] ?? [];
+        $updatedFieldName = $eventData['other']['shortname'];
 
-        if (isset($oldProfile['service_needs']) && isset($newProfile['service_needs']) && $oldProfile['service_needs'] !== $newProfile['service_needs']) {
+        if ($updatedFieldName == 'service_needs') {
             $userId = $eventData['userid'];
             $user = \core_user::get_user($userId);
 
