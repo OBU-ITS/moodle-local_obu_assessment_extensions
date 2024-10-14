399e86ddf1c9498aecd9f45cc40c34bcbcd4a1a4
Jock Coats <p0071665@brookes.ac.uk>
2024-10-14 10:50:20 +0100

    Revert plugin version and release number
    This commit reverts the plugin version from 2024101401 to 2024091101 and the release number from v0.0.4 to v0.0.3. This ensures compatibility with dependent modules that require the previous version.

--- version.php
+++ version.php
@@ -23,12 +23,12 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024101401;
+$plugin->version = 2024091101;
 $plugin->requires = 2012120301;
 $plugin->component = 'local_obu_assessment_extensions';
 $plugin->maturity = MATURITY_STABLE;
 
-$plugin->release = 'v0.0.4';
+$plugin->release = 'v0.0.3';
 $plugin->dependencies = array(
 
 );
