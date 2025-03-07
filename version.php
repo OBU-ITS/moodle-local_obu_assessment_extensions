bf3be6ce260dcbfea1d7ad56363ae04bf5dca8f8
Jock Coats <p0071665@brookes.ac.uk>
2025-03-07 05:31:53 +0000

    Update plugin version and release number
    Bumped the version to 2025030601 and updated the release to v0.1.1.2 in the plugin metadata. This ensures proper version tracking and compatibility management.

--- version.php
+++ version.php
@@ -23,12 +23,12 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025021102;
+$plugin->version = 2025030601;
 $plugin->requires = 2012120301;
 $plugin->component = 'local_obu_assessment_extensions';
 $plugin->maturity = MATURITY_STABLE;
 
-$plugin->release = 'v0.1.1.1';
+$plugin->release = 'v0.1.1.2';
 $plugin->dependencies = array(
 
 );
