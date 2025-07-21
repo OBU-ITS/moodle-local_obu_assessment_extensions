9b71d6c549d7b597e3152c112b6f3f6f45c08d33
Jock Coats <p0071665@brookes.ac.uk>
2025-07-21 18:45:17 +0100

    Update plugin version and release information
    Bump version to 2025072101 and update release to v1.0.0.1 in version.php. This ensures the plugin aligns with new updates and maintains proper version tracking.

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025041001;
+$plugin->version = 2025072101;
 $plugin->requires = 2012120301;
 $plugin->component = 'local_obu_assessment_extensions';
 $plugin->maturity = MATURITY_STABLE;
 
-$plugin->release = 'v1.0.0.0';
+$plugin->release = 'v1.0.0.1';
