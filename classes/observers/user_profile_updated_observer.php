da3b441e13a5ecc26268af5d067349dcade70109
Emir Kamel <p0092396@brookes.ac.uk>
2025-11-03 15:09:29 +0000

    adding exam extension considerations in recalc function and profile field observer

--- classes/observers/user_profile_updated_observer.php
+++ classes/observers/user_profile_updated_observer.php
@@ -42,19 +42,31 @@ class user_profile_updated_observer {
     public static function user_profile_updated_internal(\progress_trace $trace, $userId) {
         global $DB;
 
-        $sql = "SELECT uid.id, uid.data
-        FROM {user_info_data} uid
-        JOIN {user_info_field} uif ON uid.fieldid = uif.id
-        WHERE uid.userid = :userid
-        AND uif.shortname = 'extensions'";
-
+        $sql = "SELECT uif.shortname, uid.id AS dataid, uid.data
+                    FROM {user_info_data} uid
+                    JOIN {user_info_field} uif ON uid.fieldid = uif.id
+                WHERE uid.userid = :userid
+                AND uif.shortname IN ('extensions', 'exam_extension', 'exam_break')";
         $trace->output("SQL: $sql");
 
-        $userFields = $DB->get_record_sql($sql, ['userid' => $userId]);
-        $trace->output("Extensions Data: $userFields->data");
+        $userFields = $DB->get_records_sql($sql, ['userid' => $userId]);
+        $getUserFields = function(string $key) use ($userFields) {
+            return $userFields[$key] ?? null;
+        };
 
-        if ($userFields && strpos($userFields->data, '*') === 0) {
-            $user = \core_user::get_user($userId,'id, username');
+        $extensions = $getUserFields('extensions');
+        $examExtension = $getUserFields('exam_extension');
+        $examBreak = $getUserFields('exam_break');
+
+        $extensionsChanged = $extensions && is_string($extensions->data) && str_starts_with($extensions->data, '*');
+        $examExtensionChanged = $examExtension && is_string($examExtension->data) && str_starts_with($examExtension->data, '*');
+        $examBreakChanged = $examBreak && is_string($examBreak->data) && str_starts_with($examBreak->data, '*');
+
+        $user = \core_user::get_user($userId,'id, username');
+
+        if ($extensionsChanged) {
+            $trace->output('Found unprocessed extensions for user: ' . $user->username);
+
             $assessmentGroups = local_obu_assessment_ext_get_assessment_groups('by_user', $user->username);
 
             $assessments = array();
@@ -69,13 +81,45 @@ class user_profile_updated_observer {
             \core\task\manager::queue_adhoc_task($task);
             $trace->output("Task created");
 
-            $updatedIsp = ltrim($userFields->data, '*');
+            $updatedIsp = ltrim($extensions->data, '*');
             $trace->output("Updated ISP: $updatedIsp");
+
             $updatedRecord = new \stdClass();
-            $updatedRecord->id = $userFields->id;
+            $updatedRecord->id = $extensions->dataid;
             $updatedRecord->data = $updatedIsp;
             $DB->update_record('user_info_data', $updatedRecord);
             $trace->output("Complete");
+        }
+
+        if ($examExtensionChanged || $examBreakChanged) {
+            $trace->output('Found unprocessed exam extensions for user: ' . $user->username);
+
+            $assessmentGroups = local_obu_assessment_ext_get_assessment_groups('by_user', $user->username);
+
+            $exams = array();
+
+            foreach ($assessmentGroups as $group) {
+                $groupExamAssessments = local_obu_assessment_ext_get_exam_assessments_by_group($group);
+                $exams = array_merge($exams, $groupExamAssessments);
+            }
+
+            $task = new \local_obu_assessment_extensions\task\adhoc_process_user_service_needs_change();
+            $task->set_custom_data(['assessments' => $exams, 'user' => $user]);
+            \core\task\manager::queue_adhoc_task($task);
+            $trace->output("Task created");
+
+            if ($examExtensionChanged) {
+                $DB->update_record('user_info_data', (object)[
+                    'id'   => $examExtension->dataid,
+                    'data' => ltrim((string)$examExtension->data, '*'),
+                ]);
+            }
+            if ($examBreakChanged) {
+                $DB->update_record('user_info_data', (object)[
+                    'id'   => $examBreak->dataid,
+                    'data' => ltrim((string)$examBreak->data, '*'),
+                ]);
+            }
         }
     }
 }
